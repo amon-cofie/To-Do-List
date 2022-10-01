660ce0e35cf60083f15d43d0d8e8aad705a5ac0e
amon-cofie <ddd.amon123@gmail.com>
2022-10-01 12:23:47 +0000

    Add interactions

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import './style.css';
 import tasks from '../modules/tasks-array-module.js';
 import { populateTaskArray } from '../modules/create-tasks-array.js';
 import displayNode, { toDoList } from '../modules/display-task-node.js';
+import dynamicStorageUpdate from '../modules/dynamic-storage-update.js';
+import reOrderTasks from '../modules/re-order-tasks.js';
 
 const taskInput = document.querySelector('#add-new-task');
 const resetBtn = document.querySelector('#reset-button');
+const clearCompleted = document.querySelector('#clr-completed-btn');
 
 tasks.forEach((e) => {
   displayNode(e);
@@ -26,4 +29,12 @@ resetBtn.addEventListener('click', () => {
   while (toDoList.firstChild) {
     toDoList.removeChild(toDoList.lastChild);
   }
+});
+
+clearCompleted.addEventListener('click', () => {
+  const filterComplete = tasks.filter((e) => e.completed === false);
+  reOrderTasks(filterComplete);
+  dynamicStorageUpdate(filterComplete);
+
+  window.location.reload();
 });
